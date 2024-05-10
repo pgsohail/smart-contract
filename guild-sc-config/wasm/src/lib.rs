1d495c6020edb215ef0f5027baedbfe4ef13ca89
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-05-10 15:39:34 +0300

    build

--- guild-sc-config/wasm/src/lib.rs
+++ guild-sc-config/wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           17
+// Endpoints:                           22
 // Async Callback (empty):               1
-// Total number of exported functions:  19
+// Total number of exported functions:  24
 
 #![no_std]
 #![allow(internal_features)]
@@ -27,16 +27,21 @@ multiversx_sc_wasm_adapter::endpoints! {
         setUserTierApr => set_user_tier_apr
         getGuildMasterTiers => guild_master_tiers
         getUserTiers => user_tiers
-        setMaxStakedTokens => set_max_staked_tokens
         setMinUnbondEpochsUser => set_min_unbond_epochs_user
         setMinUnbondEpochsGuildMaster => set_min_unbond_epochs_guild_master
         setMinStakeUser => set_min_stake_user
         setMinStakeGuildMaster => set_min_stake_guild_master
+        setTotalStakingTokenMinted => set_total_staking_token_minted
+        increaseStakedTokens => increase_staked_tokens
+        decreaseStakedTokens => decrease_staked_tokens
+        getTotalStakedPercent => get_total_staked_percent
         getMaxStakedTokens => max_staked_tokens
         getMinUnbondEpochsUser => min_unbond_epochs_user
         getMinUnbondEpochsGuildMaster => min_unbond_epochs_guild_master
         getMinStakeUser => min_stake_user
         getMinStakeGuildMaster => min_stake_guild_master
+        getTotalStakingTokenMinted => total_staking_token_minted
+        getTotalStakingTokenStaked => total_staking_token_staked
     )
 }
 
