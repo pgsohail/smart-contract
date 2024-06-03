00d6a89ee48c1e874dffbb09c5096d49b46e5015
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-06-03 10:29:32 +0300

    remove dead code + check on merge

--- guild-sc/src/farm_base_impl/base_traits_impl.rs
+++ guild-sc/src/farm_base_impl/base_traits_impl.rs
@@ -3,8 +3,7 @@ multiversx_sc::imports!();
 use crate::config::ConfigModule;
 use crate::contexts::storage_cache::StorageCache;
 use crate::rewards::RewardsModule;
-use common_structs::{FarmToken, FarmTokenAttributes, Nonce};
-use core::marker::PhantomData;
+use common_structs::{FarmToken, Nonce};
 use fixed_supply_token::FixedSupplyToken;
 use mergeable::Mergeable;
 
@@ -28,18 +27,13 @@ pub trait FarmContract {
         + Mergeable<<Self::FarmSc as ContractBase>::Api>
         + FixedSupplyToken<<Self::FarmSc as ContractBase>::Api>
         + FarmToken<<Self::FarmSc as ContractBase>::Api>
-        + From<FarmTokenAttributes<<Self::FarmSc as ContractBase>::Api>>
-        + Into<FarmTokenAttributes<<Self::FarmSc as ContractBase>::Api>>
-        + ManagedVecItem = FarmTokenAttributes<<Self::FarmSc as ContractBase>::Api>;
+        + ManagedVecItem;
 
-    #[inline]
     fn mint_rewards(
         sc: &Self::FarmSc,
         token_id: &TokenIdentifier<<Self::FarmSc as ContractBase>::Api>,
         amount: &BigUint<<Self::FarmSc as ContractBase>::Api>,
-    ) {
-        sc.send().esdt_local_mint(token_id, 0, amount);
-    }
+    );
 
     fn calculate_per_block_rewards(
         sc: &Self::FarmSc,
@@ -59,40 +53,12 @@ pub trait FarmContract {
     fn mint_per_block_rewards(
         sc: &Self::FarmSc,
         token_id: &TokenIdentifier<<Self::FarmSc as ContractBase>::Api>,
-    ) -> BigUint<<Self::FarmSc as ContractBase>::Api> {
-        let current_block_nonce = sc.blockchain().get_block_nonce();
-        let last_reward_nonce = sc.last_reward_block_nonce().get();
-        if current_block_nonce <= last_reward_nonce {
-            return BigUint::zero();
-        }
-
-        let to_mint = Self::calculate_per_block_rewards(sc, current_block_nonce, last_reward_nonce);
-        if to_mint != 0 {
-            Self::mint_rewards(sc, token_id, &to_mint);
-        }
-
-        sc.last_reward_block_nonce().set(current_block_nonce);
-
-        to_mint
-    }
+    ) -> BigUint<<Self::FarmSc as ContractBase>::Api>;
 
     fn generate_aggregated_rewards(
         sc: &Self::FarmSc,
         storage_cache: &mut StorageCache<Self::FarmSc>,
-    ) {
-        let total_reward = Self::mint_per_block_rewards(sc, &storage_cache.reward_token_id);
-        if total_reward == 0u64 {
-            return;
-        }
-
-        storage_cache.reward_reserve += &total_reward;
-
-        if storage_cache.farm_token_supply != 0u64 {
-            let increase = (&total_reward * &storage_cache.division_safety_constant)
-                / &storage_cache.farm_token_supply;
-            storage_cache.reward_per_share += &increase;
-        }
-    }
+    );
 
     fn calculate_rewards(
         _sc: &Self::FarmSc,
@@ -115,39 +81,14 @@ pub trait FarmContract {
         caller: ManagedAddress<<Self::FarmSc as ContractBase>::Api>,
         farming_token_amount: BigUint<<Self::FarmSc as ContractBase>::Api>,
         current_reward_per_share: BigUint<<Self::FarmSc as ContractBase>::Api>,
-    ) -> Self::AttributesType {
-        let current_epoch = sc.blockchain().get_block_epoch();
-        let attributes = FarmTokenAttributes {
-            reward_per_share: current_reward_per_share,
-            entering_epoch: current_epoch,
-            compounded_reward: BigUint::zero(),
-            current_farm_amount: farming_token_amount,
-            original_owner: caller,
-        };
-
-        attributes.into()
-    }
+    ) -> Self::AttributesType;
 
     fn create_claim_rewards_initial_attributes(
         _sc: &Self::FarmSc,
         caller: ManagedAddress<<Self::FarmSc as ContractBase>::Api>,
         first_token_attributes: Self::AttributesType,
         current_reward_per_share: BigUint<<Self::FarmSc as ContractBase>::Api>,
-    ) -> Self::AttributesType {
-        let initial_attributes: FarmTokenAttributes<<Self::FarmSc as ContractBase>::Api> =
-            first_token_attributes.into();
-
-        let net_current_farm_amount = initial_attributes.get_total_supply();
-        let new_attributes = FarmTokenAttributes {
-            reward_per_share: current_reward_per_share,
-            entering_epoch: initial_attributes.entering_epoch,
-            compounded_reward: initial_attributes.compounded_reward,
-            current_farm_amount: net_current_farm_amount,
-            original_owner: caller,
-        };
-
-        new_attributes.into()
-    }
+    ) -> Self::AttributesType;
 
     fn create_compound_rewards_initial_attributes(
         sc: &Self::FarmSc,
@@ -155,23 +96,7 @@ pub trait FarmContract {
         first_token_attributes: Self::AttributesType,
         current_reward_per_share: BigUint<<Self::FarmSc as ContractBase>::Api>,
         reward: &BigUint<<Self::FarmSc as ContractBase>::Api>,
-    ) -> Self::AttributesType {
-        let initial_attributes: FarmTokenAttributes<<Self::FarmSc as ContractBase>::Api> =
-            first_token_attributes.into();
-
-        let current_epoch = sc.blockchain().get_block_epoch();
-        let new_pos_compounded_reward = initial_attributes.compounded_reward + reward;
-        let new_pos_current_farm_amount = initial_attributes.current_farm_amount + reward;
-        let new_attributes = FarmTokenAttributes {
-            reward_per_share: current_reward_per_share,
-            entering_epoch: current_epoch,
-            compounded_reward: new_pos_compounded_reward,
-            current_farm_amount: new_pos_current_farm_amount,
-            original_owner: caller,
-        };
-
-        new_attributes.into()
-    }
+    ) -> Self::AttributesType;
 
     fn get_exit_penalty(
         _sc: &Self::FarmSc,
@@ -188,18 +113,4 @@ pub trait FarmContract {
         _storage_cache: &StorageCache<Self::FarmSc>,
     ) {
     }
-}
-
-pub struct DefaultFarmWrapper<T>
-where
-    T: AllBaseFarmImplTraits,
-{
-    _phantom: PhantomData<T>,
-}
-
-impl<T> FarmContract for DefaultFarmWrapper<T>
-where
-    T: AllBaseFarmImplTraits,
-{
-    type FarmSc = T;
 }
