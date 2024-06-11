0c687a88d88c523ece2280e82c1f44549e3fc3f8
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-06-11 14:35:04 +0300

    fix issue 27

--- guild-sc/src/tokens/farm_token.rs
+++ guild-sc/src/tokens/farm_token.rs
@@ -1,8 +1,6 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
-use common_structs::Nonce;
-
 #[multiversx_sc::module]
 pub trait FarmTokenModule:
     permissions_module::PermissionsModule
@@ -54,58 +52,6 @@ pub trait FarmTokenModule:
 
     #[storage_mapper("farmTokenTransferRoleSet")]
     fn farm_token_transfer_role_set(&self) -> SingleValueMapper<bool>;
-
-    fn burn_farm_tokens_from_payments(&self, payments: &ManagedVec<EsdtTokenPayment<Self::Api>>) {
-        let mut total_amount = BigUint::zero();
-        for entry in payments.iter() {
-            total_amount += &entry.amount;
-            self.send()
-                .esdt_local_burn(&entry.token_identifier, entry.token_nonce, &entry.amount);
-        }
-
-        self.farm_token_supply().update(|x| *x -= total_amount);
-    }
-
-    fn mint_farm_tokens<T: TopEncode>(
-        &self,
-        token_id: TokenIdentifier,
-        amount: BigUint,
-        attributes: &T,
-    ) -> EsdtTokenPayment<Self::Api> {
-        let new_nonce = self
-            .send()
-            .esdt_nft_create_compact(&token_id, &amount, attributes);
-        self.farm_token_supply().update(|x| *x += &amount);
-
-        EsdtTokenPayment::new(token_id, new_nonce, amount)
-    }
-
-    fn burn_farm_tokens(&self, token_id: &TokenIdentifier, nonce: Nonce, amount: &BigUint) {
-        self.send().esdt_local_burn(token_id, nonce, amount);
-        self.farm_token_supply().update(|x| *x -= amount);
-    }
-
-    fn burn_farm_token_payment(&self, payment: &EsdtTokenPayment<Self::Api>) {
-        self.burn_farm_tokens(
-            &payment.token_identifier,
-            payment.token_nonce,
-            &payment.amount,
-        );
-    }
-
-    fn get_farm_token_attributes<T: TopDecode>(
-        &self,
-        token_id: &TokenIdentifier,
-        token_nonce: u64,
-    ) -> T {
-        let token_info = self.blockchain().get_esdt_token_data(
-            &self.blockchain().get_sc_address(),
-            token_id,
-            token_nonce,
-        );
-
-        token_info.decode_attributes()
-    }
 
     #[view(getFarmTokenId)]
     #[storage_mapper("farm_token_id")]
